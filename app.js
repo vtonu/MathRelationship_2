de52231e49fc19325f251e36e267a87e91cb8fd7
goodnight <victor.tonu@yahoo.com>
2023-08-09 22:16:17 -0700

    Added padding
    Added padding to legend and more text

--- app.js
+++ app.js
@@ -1,25 +1,41 @@
-const currentYearElement = document.getElementById('year');
-const progressBar = document.querySelector('.progress-bar');
-const completionText = document.getElementById('completion-text');
+CSS.registerProperty({
+  name: '--p',
+  syntax: '<integer>',
+  initialValue: 0,
+  inherits: true,
+});
 
-function updateYearAndGradient() {
-  const currentYear = new Date().getFullYear();
-  currentYearElement.textContent = currentYear;
+$(document).ready(function () {
+  var progressBar = $('.progress-bar .progress');
+  var counter = $('.counter');
+  var completionText = $('#completion-text');
+  var loader = $('.loader');
 
-  // Reset animation
-  progressBar.style.animation = 'none';
-  void progressBar.offsetWidth; // Trigger reflow to reset animation
-  progressBar.style.animation = 'animate 25s linear';
+  function counterInit(fValue, lValue) {
+    var counterValue = parseInt(counter.text());
 
-  if (currentYear >= 2023) {
-    completionText.textContent = 'COMPLETE';
-    progressBar.classList.add('complete'); // Add the 'complete' class to pause animation
-  } else {
-    completionText.textContent = ''; // Hide the completion text
-    progressBar.classList.remove('complete'); // Remove the 'complete' class to restart animation
+    if (counterValue >= fValue && counterValue <= lValue) {
+      counter.text(counterValue + 1);
+
+      // Calculate percentage progress
+      var percentage = ((counterValue - fValue + 1) / (lValue - fValue)) * 100;
+      progressBar.css('width', percentage + '%');
+
+      // Update the loader's data-year attribute
+      loader.attr('data-year', counterValue);
+
+      // Update the year element
+      $('#year').text(counterValue);
+
+      setTimeout(function () {
+        counterInit(fValue, lValue);
+      }, 20);
+    } else if (counterValue > lValue) {
+      completionText.text('COMPLETE');
+      completionText.show();
+      progressBar.addClass('complete');
+    }
   }
-}
 
-// Call the function initially and then at regular intervals
-updateYearAndGradient();
-setInterval(updateYearAndGradient, 25000); // Update every 25 seconds (adjust as needed)
+  counterInit(1998, 2023);
+});
